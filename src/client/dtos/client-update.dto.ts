7f59516a93919229de00b7911dd968afd6736e90
Matheus Justino <matheus.justino@ccc.ufcg.edu.br>
2021-05-01 18:40:43 -0300

    remove failed ci workflow

--- src/client/dtos/client-update.dto.ts
+++ src/client/dtos/client-update.dto.ts
@@ -1,6 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsMongoId } from 'class-validator';
+import { Type } from 'class-transformer';
+import { Types } from 'mongoose';
+
 export class ClientUpdateDto {
+	// @IsMongoId()
+	// @Type(() => Types.ObjectId)
+	// public id: Types.ObjectId;
+
 	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
